ae4eb4635610905105ef75c05b3f5459d5ae4786
ikroop <ikroop>
2020-03-03 21:00:42 -0500

    [IKROOP] PICKER VIEW

--- Passingdata/FirstViewController.swift
+++ Passingdata/FirstViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    
 
     
     @IBOutlet weak var txtName: UITextField!
+    
+    
+    @IBOutlet weak var pickerCountry: UIPickerView!
+    var countryList = ["India", "Canada", "USA", "UK", "Sri Lanka", "Nepal", "pakistan"]
+    var codeList = ["IN", "CA", "US", "UK", "SL", "NL", "PK"]
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pickerCountry.delegate = self
+        pickerCountry.dataSource = self
+        
     }
 
     
@@ -31,6 +44,32 @@ class FirstViewController: UIViewController {
         
         
     }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return self.countryList.count
+        }
+        return self.codeList.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return countryList[row]
+        }
+        return codeList[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 0
+        {
+        print(countryList[row])
+    }
+        else
+        {
+            print(codeList[row])
+        }
     
 }
 
+}
